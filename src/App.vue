8cc63e2c0d53d5855d815596be7ea2d38aa7e25d
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2019-09-12 11:50:47 +0200

    Add working snap toggle

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-    <div id="slider">
+    <div id="slider" v-bind:class="{'slider-snap': snapScroll}">
 
-      <section id="AboutPage">
-        <AboutPage></AboutPage>
-      </section>
-
-      <section id="StationPage">
-        <StationPage v-bind:data="info"></StationPage>
-      </section>
-
-      <section id="SchedulePage">
-        <SchedulePage></SchedulePage>
-      </section>
+      <AboutPage id="about-page"></AboutPage>
+      <StationPage id="station-page"></StationPage>
+      <SchedulePage id="schedule-page"></SchedulePage>
 
     </div>
-    <TabBar></TabBar>
+    <TabBar v-on:toggleSnap="toggleSnapScroll"></TabBar>
   </div>
 </template>
 
@@ -29,7 +21,7 @@ export default {
   name: "app",
   data () {
     return {
-      info: null
+      snapScroll: true
     }
   },
   components: {
@@ -40,24 +32,22 @@ export default {
   },
 
   methods:{
-     scrollToStationPage: function() {
-       this.$scrollTo("#StationPage", 0, { container: "#slider", cancelable: false, force: true, y: false, x: true} );
-     }
+    toggleSnapScroll: function (boolean) {
+      this.snapScroll = boolean;
+    }
  },
- mounted(){
-    this.scrollToStationPage();
 
-    this.$http
-      .get("https://tohru.sylvanas.dream/v1/origins/")
-      .then(response => (this.info = response.data.payload))
- },
+  mounted(){
+    this.$scrollTo("#station-page", 0, { container: "#slider", cancelable: false, force: true, y: false, x: true});
+  }
 };
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
 
-* {
+/* CSS Reset */
+html {
   box-sizing: border-box;
 }
 
@@ -65,35 +55,49 @@ body {
   margin: 0;
   overflow: hidden;
 	background-color: #131313;
-
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
 }
 
-.page {
-  height: 100vh;
-  padding: 20px;
+/* Global Text Style */
+p {
+  font-family: "Montserrat", sans-serif;
+  color: #F8F8F8;
+  font-size: 15px;
+  font-weight: 500;
 }
 
+h1 {
+  font-family: "Montserrat", sans-serif;
+  color: #F8F8F8;
+  font-size: 18px;
+}
+
+h2 {
+  font-family: "Montserrat", sans-serif;
+  color: #F8F8F8;
+  font-size: 23px;
+}
+
+/* Slider, Pages */
 #slider {
-	scroll-snap-type: x mandatory;
-	display: flex;
-	overflow-x: scroll;
+  overflow-x: scroll;
+  display: flex;
 }
 
-#slider section {
-	border: 0;
+.slider-snap {
+  scroll-snap-type: x mandatory;
+}
+
+#about-page, #station-page, #schedule-page {
+  border: 0;
+  height: 100vh;
 	min-width: 100vw;
-	height: 100vh;
   scroll-snap-stop: always;
-	scroll-snap-align: center;
-	position: relative;
+  scroll-snap-align: center;
 }
 
 @media screen and (min-width: 900px) and (orientation: landscape){
-  #slider section {
+  #about-page, #station-page, #schedule-page {
   	min-width: 33.33vw;
   }
 }
-
 </style>
--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class=main>
-    <button v-on:click.stop="scroll('AboutPage')">About</button>
-    <button v-on:click.stop="scroll('StationPage')">Station</button>
-    <button v-on:click.stop="scroll('SchedulePage')">Schedule</button>
+    <button v-on:click.stop="scroll('#about-page')">About</button>
+    <button v-on:click.stop="scroll('#station-page')">Station</button>
+    <button v-on:click.stop="scroll('#schedule-page')">Schedule</button>
   </div>
 </template>
 
@@ -10,27 +10,40 @@
 export default {
   name: "TabBar",
   methods: {
-   scroll: function (message) {
-     this.$scrollTo("#" + message, 0, { container: "#slider", cancelable: false, y: false, x: true} );
-   }
- }
+    scroll: function (target) {
+      var self = this;
+      this.$emit("toggleSnap", false);
+
+      this.$scrollTo(target, 500, {
+        container: "#slider",
+        easing: "ease",
+        cancelable: true,
+        y: false,
+        x: true,
+
+        onDone: function(element) {
+          self.$emit("toggleSnap", true)},
+
+        onCancel: function() {
+          self.$emit("toggleSnap", true)}
+      });
+    }}
 };
 </script>
 
 <style scoped>
 p {
-  font-family: "Montserrat", sans-serif;
   text-align: center;
-  color: #000000;
 }
 
 button {
     width: 33.3%;
     font-family: "Montserrat", sans-serif;
     color: #F8F8F8;
+    font-weight: 500;
+    font-size: 15px;
     height: 100%;
     border: none;
-    border-top: 5px;
     background-color: Transparent;
     text-align: center;
     text-decoration: none;
