2ce73d3195e20972164b60cc490da2775c071c72
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-02-06 20:54:40 +0100

    Refactor parser

--- parser/parser/config.py
+++ parser/parser/config.py
@@ -1,12 +1,9 @@
 import os
 from datetime import datetime
 
+
 class Config:
     def __init__(self):
-        """Tohru Parser configuration.
-
-        Gets config from Docker container environment variables.
-        """
         self.redis_hostname = os.environ.get("REDIS_HOSTNAME") or "redis"
 
         self.filename = os.environ.get("FILENAME") or "timetable.xlsx"
